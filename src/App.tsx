82778fc898acea7c4c344f88465ffff6afeb2c70
Muhammad Amman <developercity316@gmail.com>
2024-07-25 18:38:36 +0500

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
 import './App.css';
 import { Unity, useUnityContext } from "react-unity-webgl";
+import { useEffect, useState } from "react";
 
 function App() {
   const { unityProvider } = useUnityContext({
-    loaderUrl: "build/Webgl.loader.js",
-    dataUrl: "build/Webgl.data",
-    frameworkUrl: "build/Webgl.framework.js",
-    codeUrl: "build/Webgl.wasm",
+    loaderUrl: "build/Trump Runner.loader.js",
+    dataUrl: "build/Trump Runner.data",
+    frameworkUrl: "build/Trump Runner.framework.js",
+    codeUrl: "build/Trump Runner.wasm",
   });
 
+  const [windowDimensions, setWindowDimensions] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
-    <div>
+    <div className="App">
       <Unity
         style={{
-          width: "80%",
-
-          justifySelf: "center",
-          alignSelf: "center",
+          width: `${windowDimensions.width}px`,
+          height: `${windowDimensions.height}px`,
         }}
         unityProvider={unityProvider}
       />
     </div>
   );
 }
+
 export default App;
