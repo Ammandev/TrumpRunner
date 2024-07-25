80124096c58a1cf7ba0eb688ea5951f4f19c9c35
Muhammad Amman <developercity316@gmail.com>
2024-07-25 18:27:20 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/vite-deploy/"
-})
+  base: '/TelegramBot/', // Set this to your repository name
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
+});
